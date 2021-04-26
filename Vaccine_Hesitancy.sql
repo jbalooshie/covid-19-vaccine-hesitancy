4a3349bcb924f9c94155684e098d71b9ffbba7f6
MonikaSData <mspiritova@gwmail.gwu.edu>
2021-04-25 22:55:20 -0400

    Adding final version of DB and related documents

--- Vaccine_Hesitancy.sql
+++ Vaccine_Hesitancy.sql
@@ -8,11 +8,11 @@ CREATE TABLE "vaccine_hesitancy_covid" (
     "state" varchar   NOT NULL,
     "est_hesitant" decimal   NOT NULL,
     "est_strongly_hesitant" decimal   NOT NULL,
-    "svi" decimal   NOT NULL,
+    "svi" decimal   NULL,
     "svi_category" varchar   NOT NULL,
     "cvac" decimal   NOT NULL,
     "cvac_category" varchar   NOT NULL,
-    "percent_fully_vaccinated" decimal   NOT NULL,
+    "percent_fully_vaccinated" decimal   NULL,
     "percent_hispanic" decimal   NOT NULL,
     "percent_american_indian_alaska_native" decimal   NOT NULL,
     "percent_asian" decimal   NOT NULL,
@@ -21,75 +21,67 @@ CREATE TABLE "vaccine_hesitancy_covid" (
     "percent_white" decimal   NOT NULL,
     "geographical_point" varchar   NOT NULL,
     "state_code" varchar   NOT NULL,
-    "county_boundary" varchar   NOT NULL,
-    "state_boundary" varchar   NOT NULL,
+    "county_boundary" varchar   NULL,
+    "state_boundary" varchar   NULL,
     CONSTRAINT "pk_vaccine_hesitancy_covid" PRIMARY KEY (
         "fips_code"
      )
 );
 
-CREATE TABLE "county_state" (
-    "county" varchar   NOT NULL,
-    "state" varchar   NOT NULL,
-    "fips_code" int   NOT NULL,
-    CONSTRAINT "pk_county_state" PRIMARY KEY (
-        "county","state"
-     )
-);
-
 CREATE TABLE "county_statistics" (
+    "fips_code" int   NOT NULL,
     "county" varchar   NOT NULL,
     "state" varchar   NOT NULL,
-    "percentage16_Donald_Trump" decimal   NOT NULL,
-    "percentage16_Hillary_Clinton" decimal   NOT NULL,
-    "total_votes16" decimal   NOT NULL,
-    "votes16_Donald_Trump" decimal   NOT NULL,
-    "votes16_Hillary_Clinton" decimal   NOT NULL,
-    "percentage20_Donald_Trump" decimal   NOT NULL,
-    "percentage20_Joe_Biden" decimal   NOT NULL,
-    "total_votes20" decimal   NOT NULL,
-    "votes20_Donald_Trump" decimal   NOT NULL,
-    "votes20_Joe_Biden" decimal   NOT NULL,
-    "lat" decimal   NOT NULL,
-    "long" decimal   NOT NULL,
-    "cases" decimal   NOT NULL,
-    "deaths" decimal   NOT NULL,
-    "TotalPop" decimal   NOT NULL,
-    "Men" decimal   NOT NULL,
-    "Women" decimal   NOT NULL,
-    "Hispanic" decimal   NOT NULL,
-    "White" decimal   NOT NULL,
-    "Black" decimal   NOT NULL,
-    "Native" decimal   NOT NULL,
-    "Asian" decimal   NOT NULL,
-    "Pacific" decimal   NOT NULL,
-    "VotingAgeCitizen" decimal   NOT NULL,
-    "Income" decimal   NOT NULL,
-    "IncomeErr" decimal   NOT NULL,
-    "IncomePerCap" decimal   NOT NULL,
-    "IncomePerCapErr" decimal   NOT NULL,
-    "Poverty" decimal   NOT NULL,
-    "ChildPoverty" decimal   NOT NULL,
-    "Professional" decimal   NOT NULL,
-    "Service" decimal   NOT NULL,
-    "Office" decimal   NOT NULL,
-    "Construction" decimal   NOT NULL,
-    "Production" decimal   NOT NULL,
-    "Drive" decimal   NOT NULL,
-    "Carpool" decimal   NOT NULL,
-    "Transit" decimal   NOT NULL,
-    "Walk" decimal   NOT NULL,
-    "OtherTransp" decimal   NOT NULL,
-    "WorkAtHome" decimal   NOT NULL,
-    "MeanCommute" decimal   NOT NULL,
-    "Employed" decimal   NOT NULL,
-    "PrivateWork" decimal   NOT NULL,
-    "PublicWork" decimal   NOT NULL,
-    "SelfEmployed" decimal   NOT NULL,
-    "FamilyWork" decimal   NOT NULL,
-    "Unemployment" decimal   NOT NULL,
+    "percentage16_Donald_Trump" decimal   NULL,
+    "percentage16_Hillary_Clinton" decimal   NULL,
+    "total_votes16" decimal   NULL,
+    "votes16_Donald_Trump" decimal   NULL,
+    "votes16_Hillary_Clinton" decimal   NULL,
+    "percentage20_Donald_Trump" decimal   NULL,
+    "percentage20_Joe_Biden" decimal   NULL,
+    "total_votes20" decimal   NULL,
+    "votes20_Donald_Trump" decimal   NULL,
+    "votes20_Joe_Biden" decimal   NULL,
+    "lat" decimal   NULL,
+    "long" decimal   NULL,
+    "cases" decimal   NULL,
+    "deaths" decimal   NULL,
+    "TotalPop" decimal   NULL,
+    "Men" decimal   NULL,
+    "Women" decimal   NULL,
+    "Hispanic" decimal   NULL,
+    "White" decimal   NULL,
+    "Black" decimal   NULL,
+    "Native" decimal   NULL,
+    "Asian" decimal   NULL,
+    "Pacific" decimal   NULL,
+    "VotingAgeCitizen" decimal   NULL,
+    "Income" decimal   NULL,
+    "IncomeErr" decimal   NULL,
+    "IncomePerCap" decimal   NULL,
+    "IncomePerCapErr" decimal   NULL,
+    "Poverty" decimal   NULL,
+    "ChildPoverty" decimal   NULL,
+    "Professional" decimal   NULL,
+    "Service" decimal   NULL,
+    "Office" decimal   NULL,
+    "Construction" decimal   NULL,
+    "Production" decimal   NULL,
+    "Drive" decimal   NULL,
+    "Carpool" decimal   NULL,
+    "Transit" decimal   NULL,
+    "Walk" decimal   NULL,
+    "OtherTransp" decimal   NULL,
+    "WorkAtHome" decimal   NULL,
+    "MeanCommute" decimal   NULL,
+    "Employed" decimal   NULL,
+    "PrivateWork" decimal   NULL,
+    "PublicWork" decimal   NULL,
+    "SelfEmployed" decimal   NULL,
+    "FamilyWork" decimal   NULL,
+    "Unemployment" decimal   NULL,
     CONSTRAINT "pk_county_statistics" PRIMARY KEY (
-        "county","state"
+        "fips_code"
      )
 );
 
@@ -112,7 +104,7 @@ CREATE TABLE "census_demographic" (
     "IncomePerCap" int   NOT NULL,
     "IncomePerCapErr" int   NOT NULL,
     "Poverty" decimal   NOT NULL,
-    "ChildPoverty" decimal   NOT NULL,
+    "ChildPoverty" decimal   NULL,
     "Professional" decimal   NOT NULL,
     "Service" decimal   NOT NULL,
     "Office" decimal   NOT NULL,
@@ -136,12 +128,6 @@ CREATE TABLE "census_demographic" (
      )
 );
 
-ALTER TABLE "county_state" ADD CONSTRAINT "fk_county_state_fips_code" FOREIGN KEY("fips_code")
-REFERENCES "vaccine_hesitancy_covid" ("fips_code");
-
-ALTER TABLE "county_statistics" ADD CONSTRAINT "fk_county_statistics_county_state" FOREIGN KEY("county", "state")
-REFERENCES "county_state" ("county", "state");
-
-ALTER TABLE "census_demographic" ADD CONSTRAINT "fk_census_demographic_CountyId" FOREIGN KEY("CountyId")
-REFERENCES "vaccine_hesitancy_covid" ("fips_code");
+ALTER TABLE "vaccine_hesitancy_covid" ADD CONSTRAINT "fk_vaccine_hesitancy_covid_fips_code" FOREIGN KEY("fips_code")
+REFERENCES "census_demographic" ("CountyId");
 
